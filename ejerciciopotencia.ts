8db246726a266017f271148eb3cc4031397116ba
Andreafull2024 <168041677+Andreafull2024@users.noreply.github.com>
2024-05-09 14:44:24 -0300

    Add files via upload

---
+++ ejerciciopotencia.ts
@@ -0,0 +1,24 @@
+import * as readlineSync from 'readline-sync';
+
+let base : number = readlineSync.questionInt("Ingrese el número de la base: "); 
+let exponente : number = readlineSync.questionInt("Ingrese el número del exponente: "); 
+const resultado= calcularPotencia (base,exponente)
+
+function calcularPotencia (base:number,exponente:number,):number {
+    
+    if (exponente === 0) {
+        return 1;
+    } else {
+        let resultado = 1;
+        for (let i = 0; i < exponente; i++) {
+            resultado *= base;
+        }
+        return resultado;
+    }
+}
+
+if (exponente < 0) {
+    console.log('El exponente debe ser mayor o igual a cero.');
+} else {
+    const resultado = calcularPotencia (base,exponente);
+    console.log ('El resultado de {base}^{exponente} es: {resultado}')
