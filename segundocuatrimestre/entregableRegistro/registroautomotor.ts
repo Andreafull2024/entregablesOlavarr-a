197b1c86d5856bb7ec50a1a37af1b87083766bb8
Andreafullstack <andreasanjuan21@gmail.com>
2024-10-06 14:58:04 -0300

    Registro

--- segundocuatrimestre/entregableRegistro/registroautomotor.ts
+++ segundocuatrimestre/entregableRegistro/registroautomotor.ts
@@ -4,7 +4,7 @@ import { Moto } from './moto';
 import { Camion } from './camion';
 
 export class RegistroAutomotor {
-    private vehiculos: (Auto | Moto | Camion)[] = [];
+    private vehiculos: (Auto | Moto | Camion | null)[] = [];
 
     public agregarVehiculo(vehiculo: Auto | Moto | Camion): void {
         this.vehiculos.push(vehiculo);
@@ -13,20 +13,16 @@ export class RegistroAutomotor {
     public modificarVehiculo(index: number, vehiculo: Auto | Moto | Camion): void {
         if (index >= 0 && index < this.vehiculos.length) {
             this.vehiculos[index] = vehiculo;
-        } else {
-            throw new Error("Índice fuera de rango");
         }
     }
 
     public darDeBaja(index: number): void {
         if (index >= 0 && index < this.vehiculos.length) {
-            this.vehiculos.splice(index, 1);
-        } else {
-            throw new Error("Índice fuera de rango");
+            this.vehiculos[index] = null; 
         }
     }
 
-    public getVehiculos(): (Auto | Moto | Camion)[] {
+    public getVehiculos(): (Auto | Moto | Camion | null)[] {
         return this.vehiculos;
     }
 }
